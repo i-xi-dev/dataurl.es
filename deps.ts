9c3f2e573f9f9eeddbcf19be7ee3494248eb92c4
i-xi-dev <naoyuki.isono@gmail.com>
2023-08-14 06:22:08 +0900

    2.0.7

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { StringUtils } from "https://raw.githubusercontent.com/i-xi-dev/str.es/2.0.0/mod.ts";
-export { HttpUtils } from "https://raw.githubusercontent.com/i-xi-dev/http-utils.es/2.1.6/mod.ts";
-export { Isomorphic } from "https://raw.githubusercontent.com/i-xi-dev/isomorphic.es/2.0.5/mod.ts";
-export { Percent } from "https://raw.githubusercontent.com/i-xi-dev/percent.es/4.0.18/mod.ts";
-export { Base64 } from "https://raw.githubusercontent.com/i-xi-dev/base64.es/4.0.3/mod.ts";
-export { MediaType } from "https://raw.githubusercontent.com/i-xi-dev/mimetype.es/1.2.6/mod.ts";
+export { StringUtils } from "https://raw.githubusercontent.com/i-xi-dev/str.es/2.0.1/mod.ts";
+export { HttpUtils } from "https://raw.githubusercontent.com/i-xi-dev/http-utils.es/2.1.7/mod.ts";
+export { Isomorphic } from "https://raw.githubusercontent.com/i-xi-dev/isomorphic.es/2.0.6/mod.ts";
+export { Percent } from "https://raw.githubusercontent.com/i-xi-dev/percent.es/4.0.19/mod.ts";
+export { Base64 } from "https://raw.githubusercontent.com/i-xi-dev/base64.es/4.0.4/mod.ts";
+export { MediaType } from "https://raw.githubusercontent.com/i-xi-dev/mimetype.es/1.2.7/mod.ts";
