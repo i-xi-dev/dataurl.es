199c19e472682668a21c4de5392b0e48348423b4
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-07 00:40:09 +0900

    2.0.17

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * as StringEx from "https://raw.githubusercontent.com/i-xi-dev/string.es/3.6.0/mod.ts";
-export { HttpUtils } from "https://raw.githubusercontent.com/i-xi-dev/http-utils.es/2.1.17/mod.ts";
-export { Percent } from "https://raw.githubusercontent.com/i-xi-dev/percent.es/4.0.29/mod.ts";
-export { Base64 } from "https://raw.githubusercontent.com/i-xi-dev/base64.es/4.1.6/mod.ts";
-export { MediaType } from "https://raw.githubusercontent.com/i-xi-dev/mimetype.es/1.2.15/mod.ts";
+export * as StringEx from "https://raw.githubusercontent.com/i-xi-dev/string.es/4.0.2/mod.ts";
+export { HttpUtils } from "https://raw.githubusercontent.com/i-xi-dev/http-utils.es/2.1.18/mod.ts";
+export { Percent } from "https://raw.githubusercontent.com/i-xi-dev/percent.es/4.0.30/mod.ts";
+export { Base64 } from "https://raw.githubusercontent.com/i-xi-dev/base64.es/4.1.7/mod.ts";
+export { MediaType } from "https://raw.githubusercontent.com/i-xi-dev/mimetype.es/1.2.16/mod.ts";
